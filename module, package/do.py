835613ba146656175b1eeb77e1692e0c479d45e3
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2021-08-30 12:08:13 +0900

    package 2/2

--- module, package/do.py
+++ module, package/do.py
@@ -1,3 +1,25 @@
-import my_module
+# import my_module
+# my_module.나라친구()
 
-my_module.나라친구()
+# import tour.jeonnam
+# tour.jeonnam.채리()
+#
+# from tour import jeonnam
+# jeonnam.채리()
+#
+# from tour.jeonnam import 채리
+# 채리()
+
+# import tour.jeonnam as tj
+# tj.채리()
+#
+# from tour import jeonnam as je
+# je.채리()
+#
+# from tour.jeonnam import 채리 as cherry
+# cherry()
+
+from tour import *
+jeonnam.채리()            #__init__에 __all__에 넣어야 실행 가능
+gp = gangwondo.GangwondoPackage()
+print(gp)
