b4c13ff9c4e972fa3daed157289b426a52f8b186
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2021-09-10 15:27:33 +0900

    야구게임엔진 완성

---
+++ module, package/tour/baseball.py
@@ -0,0 +1,15 @@
+answer = make_answer()
+print(answer)
+
+# 반복
+while True:
+    # 질문
+    guess = input('예상 숫자 : ')
+    # 결과 판정
+    strike, ball = check(guess, answer)
+    # 출력
+    print(f'{guess}\tstrike: {strike}, ball: {ball}')
+    # 정답 == 숫자, 반복 종료
+    if answer == guess:
+        print('정답입니다.')
+        break
