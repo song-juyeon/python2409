632222787e60f8a7033e5b9e5d7d430fe7db7d54
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2021-09-13 13:39:43 +0900

    package

---
+++ module, package/run_package.py
@@ -0,0 +1,12 @@
+from foods.fruits import orange
+orange.eat()
+
+from foods.fruits import *
+
+from foods.fruits import orange as o
+
+from foods.fruits.orange import eat
+
+from foods.fruits.orange import *
+
+import foods.fruits.orange
