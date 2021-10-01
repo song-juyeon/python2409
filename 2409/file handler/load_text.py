e28723222594288b281e05e8741f7a549e7eb4a7
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2021-10-01 15:30:38 +0900

    ** load text, readline, split

--- 2409/file handler/load_text.py
+++ 2409/file handler/load_text.py
@@ -19,3 +19,17 @@ lines = f.readlines()
 f.close()
 for line in lines:
     print(line.rstrip())
+
+# quiz
+# 이름: 이유빈 [tab] 좋아하는 색: 초록색
+# 이름: 김효진 [tab] 좋아하는 색: 하늘색
+print('---------------------\n퀴즈')
+f = open('text.txt', 'r', encoding='utf-8')
+lines = f.readlines()
+f.close()
+for line in lines:
+    # line = '고에스더:검은색'
+    data = line.split(':')
+
+    # print('이름:' + line.rstrip()[:3] + '\t좋아하는 색: ' + line.rstrip()[4:])
+    print('이름:' + data[0] + '\t좋아하는 색: ' + data[1])
